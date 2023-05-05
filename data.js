45e1955a4c624a58914ad004d975b6f954b47e76
RadaRish <rada.rish@ya.ru>
2023-05-05 15:41:02 +0300

    pano greenfeel demo

---
+++ data.js
@@ -0,0 +1,181 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "номер комфорт",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.877070817122222,
+        "pitch": -0.03846794012978805,
+        "fov": 1.4028980174571706
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9472979362484857,
+          "pitch": 0.191667342056844,
+          "rotation": 0,
+          "target": "1---1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1---1",
+      "name": "номер люкс 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.12220634288036791,
+          "pitch": 0.13820651033932307,
+          "rotation": 0,
+          "target": "2---2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2---2",
+      "name": "номер люкс 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.977672494005464,
+        "pitch": 0.08476483668332868,
+        "fov": 1.4028980174571706
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.040916556575634644,
+          "pitch": 0.13139351523444986,
+          "rotation": 0,
+          "target": "1---1"
+        },
+        {
+          "yaw": -2.8471608851971144,
+          "pitch": 0.20615573675641485,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": 3.0147203391316486,
+          "pitch": 0.20837844779897807,
+          "rotation": 0,
+          "target": "0--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "номер стандарт",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.3683954129157474,
+        "pitch": 0.0458637827833126,
+        "fov": 1.4028980174571706
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2925351469002564,
+          "pitch": 0.14432910764382711,
+          "rotation": 0,
+          "target": "2---2"
+        },
+        {
+          "yaw": 1.5668476451646711,
+          "pitch": 0.14193712846690687,
+          "rotation": 0,
+          "target": "0--"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
